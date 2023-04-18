cb39ca9178b2ed5d5ebb6700c66790b833cb717f
pgm-dirkbouckaert <dirk.bouckaert@student.arteveldehs.be>
2023-04-18 19:03:01 +0200

    Finish project page

--- src/js/app.js
+++ src/js/app.js
@@ -8,14 +8,21 @@ import { navItems, projects } from './data.js';
       this.cacheElements();
       this.buildNav();
       this.buildProjects();
+      this.listenForShowProjectDetails();
+      this.listenForCloseModal();
     },
     cacheElements() {
-      this.$pageInfo = document.getElementById('page-info');
-      this.$navList = document.getElementById('nav-list');
+      this.$modal = document.getElementById('modal');
+      this.$btnCloseModal = document.getElementById('btn-close-modal');
+      this.$modalBody = document.getElementById('modal-body');
+      this.$navbar = document.getElementById('navbar');
       this.$projects = document.getElementById('projects');
     },
+    /**
+     * NAVBAR
+     */
     buildNav() {
-      this.$navList.innerHTML = this.renderNav(navItems, this.path);
+      this.$navbar.innerHTML = this.renderNav(navItems, this.path);
     },
     renderNav(navItems, path) {
       return navItems
@@ -30,10 +37,12 @@ import { navItems, projects } from './data.js';
         })
         .join('');
     },
+    /**
+     * PROJECTS
+     */
     buildProjects() {
       if (this.path === 'projects.html') {
         const categories = [...new Set(projects.map((p) => p.category))];
-        console.log('categories:', categories);
         this.$projects.innerHTML = this.renderProjects(categories);
       }
     },
@@ -51,9 +60,9 @@ import { navItems, projects } from './data.js';
                   return `
                   <li class="project-card">
                     <div class="tooltip">${project.summary}</div>
-                    <a href="" data-id="${project.id}">
+                    <a href="" data-project-id="${project.id}">
                       <img src="images/${project.image}" alt="logo" />
-                      <h5>${project.title}</h5>
+                      <h5>${project.title.replace('NT2', 'NT2<br>')}</h5>
                     </a>
                   </li>`;
                 })
@@ -62,6 +71,66 @@ import { navItems, projects } from './data.js';
           `;
         })
         .join('');
+    },
+    /**
+     * PROJECT DETAILS
+     */
+    listenForShowProjectDetails() {
+      const links = document.querySelectorAll('.project-card a');
+      for (const link of links) {
+        link.addEventListener('click', (e) => {
+          e.preventDefault();
+          const projectId = parseInt(e.currentTarget.dataset.projectId, 10);
+          const project = projects.find((p) => p.id === projectId);
+          if (!project) {
+            return this.showModal({
+              type: 'error',
+              message: 'Project niet gevonden.',
+            });
+          }
+          this.showModal({
+            type: 'info',
+            message: this.renderProjectDetails(project),
+          });
+        });
+      }
+    },
+    renderProjectDetails(project) {
+      return `
+        <h3>${project.title}</h3>
+        <div class="text-muted">Categorie: ${project.category}</div>
+        <div class="project-summary">${project.summary}</div>
+        <div class="project-details">
+          <ul>
+            ${project.details
+              .split('|')
+              .map((item) => `<li>${item.trim()}</li>`)
+              .join('')}
+          </ul>  
+        </div>
+        <div class="project-link">
+          <a href="${
+            project.link
+          }" target="_blank" title="Open in een nieuw venster">
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+        </div>
+      `;
+    },
+    /**
+     * MODAL
+     */
+    showModal(options) {
+      this.$modalBody.innerHTML = options.message;
+      this.$modal.classList.add(options.type);
+      this.$modal.classList.remove('hidden');
+    },
+    listenForCloseModal() {
+      if (this.$modal) {
+        this.$btnCloseModal.addEventListener('click', (e) => {
+          this.$modal.setAttribute('class', 'modal hidden');
+        });
+      }
     },
   };
   app.init();
--- src/js/data.js
+++ src/js/data.js
@@ -13,7 +13,9 @@ export const projects = [
     image: 'ileren.png',
     link: 'https://ileren.be/',
     summary: 'Edutools en online apps',
-    details: '',
+    details: `Website met educatieve online tools.
+    | Bv. kruiswoordpuzzel, woordzoeker, galgje, waagstuk, mindmaps, ...'
+    | HTML, CSS, Bootstrap, Javascript, JQuery.`,
   },
   {
     id: 2,
@@ -22,7 +24,10 @@ export const projects = [
     image: 'schuilplaats.png',
     link: 'https://schuilplaats.be/',
     summary: 'Muziekstreaming',
-    details: '',
+    details: `Website waarop online muziek gestreamd wordt.
+    | De albums kunnen gedownload worden.
+    | Er kunnen ook een aantal teksten en partituren gedownload worden.
+    | HTML, CSS, Bootstrap, Javascript.`,
   },
   {
     id: 3,
@@ -31,16 +36,21 @@ export const projects = [
     image: 'nt2grammatica.png',
     link: 'https://cvogent.be/nt2grammatica',
     summary: 'Grammatica voor anderstaligen',
-    details: '',
+    details: `Website met een online grammatica voor anderstalligen.
+    | Voor de cursisten van CVO GENT.
+    | HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 4,
     category: 'HTML/CSS/JS',
     title: 'Digiboek',
     image: 'nt2digiboek.png',
-    link: 'https://dirkb.be/portfolio/nt2digiboek-sample/',
+    link: 'projects/nt2digiboek-sample/',
     summary: 'Online handboeken',
-    details: '',
+    details: `Digiboek voor de leerkrachten van CVO GENT. 
+    | Voor dit portfolio kan je een extract bekijken met één handboek.
+    | Het origineel biedt alle handboeken aan.
+    | HTML, CSS, Bootstrap, Javascript, JQuery, canvas`,
   },
   {
     id: 5,
@@ -49,7 +59,8 @@ export const projects = [
     image: 'mimosa.png',
     link: 'https://dirkb.be/mimosa',
     summary: 'Fotogallerij',
-    details: 'Farmstand',
+    details: `Op deze website wordt een fotogallerij aangeboden.
+    | Eigen MVC met PHP, Bootstrap 5, MySQL, gebruikersbeheer.`,
   },
   {
     id: 6,
@@ -58,7 +69,8 @@ export const projects = [
     image: 'farmstand.png',
     link: 'https://dirkb.be/farmstand',
     summary: 'Oefenproject met eigen MVC-framework.',
-    details: '',
+    details: `Gebaseerd op de NodeJS versie van Colt Steele.
+    | Eigen MVC met PHP, Bootstrap 5, MySQL, gebruikersbeheer.`,
   },
   {
     id: 7,
@@ -67,7 +79,9 @@ export const projects = [
     image: 'bestellingen-voeding.png',
     link: 'https://cvogent.be/bestellingen-voeding',
     summary: 'Oefenproject met eigen MVC-framework.',
-    details: '',
+    details: `Ontwerp voor CVO GENT.
+    | Geen toegang zonder cvogent-account.
+    | Eigen MVC met PHP, Bootstrap 5, MySQL, gebruikersbeheer.`,
   },
   {
     id: 8,
@@ -76,43 +90,54 @@ export const projects = [
     image: 'cvobib.png',
     link: 'https://cvogent.be/cvobib/',
     summary: 'Bibliotheekapplicatie',
-    details: '',
+    details: `App voor de bibliotheek van CVO GENT.
+    | Publiek gedeelte voor de cursisten.
+    | Afgeschermd gedeelte enkel voor personeel van CVO GENT.
+    | PHP MVC met CodeIgniter 4.`,
   },
   {
     id: 9,
     category: 'Android Apps',
-    title: 'NT2<br>Grammatica',
+    title: 'NT2 Grammatica',
     image: 'nt2grammatica-android.png',
     link: 'https://play.google.com/store/apps/details?id=be.ileren.grammatica',
     summary: 'Grammatica voor anderstaligen',
-    details: '',
+    details: `Android app die een grammatica voor anderstalligen aanbiedt.
+    | Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 10,
     category: 'Android Apps',
-    title: 'NT2<br>Werkwoorden',
+    title: 'NT2 Werkwoorden',
     image: 'nt2werkwoorden.png',
     link: 'https://play.google.com/store/apps/details?id=be.ileren.werkwoorden',
     summary: 'Studeer onregelmatige werkwoorden',
-    details: '',
+    details: `Android app waarmee je de onregelmatige werkwoorden kan studeren.
+    | Bedoeld voor anderstaligen.
+    | Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 11,
     category: 'Android Apps',
-    title: 'NT2<br>Voorzetsels',
+    title: 'NT2 Voorzetsels',
     image: 'nt2voorzetsels.png',
     link: 'https://play.google.com/store/apps/details?id=be.ileren.voorzetsels',
     summary: 'Studeer voorzetsels',
-    details: '',
+    details: `Android app waarmee je de vaste voorzetsels kan inoefenen.
+    | Bedoeld voor anderstaligen.
+    | Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 12,
     category: 'Android Apps',
-    title: 'NT2<br>Eten en Drinken',
+    title: 'NT2 Eten en Drinken',
     image: 'nt2etenendrinken.png',
     link: 'https://play.google.com/store/apps/details?id=ileren.be.nt2etenendrinken',
     summary: 'Studeer woordenschat',
-    details: '',
+    details: `Met deze app kan je nieuwe woorden leren over eten en drinken.
+    | Inclusief audio.
+    | Bedoeld voor anderstaligen.
+    | Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 13,
@@ -121,7 +146,8 @@ export const projects = [
     image: 'medischrekenen.png',
     link: 'https://play.google.com/store/apps/details?id=appinventor.ai_diboma.MedischRekenen',
     summary: 'Maak medische berekeningen',
-    details: '',
+    details: `Met deze app kan je verschillende medische berekeningen maken.
+    | App Inventor, Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 14,
@@ -130,7 +156,8 @@ export const projects = [
     image: 'zuurbase.png',
     link: 'https://play.google.com/store/apps/details?id=be.ileren.zuurbase',
     summary: 'Voedingslijst met zuur-base waarden',
-    details: '',
+    details: `Deze app biedt een lijst aan waarin je de zuur-base waarden kan raadplegen.
+    | Android Studio, Cordova, HTML, CSS, Bootstrap, Javascript`,
   },
   {
     id: 15,
@@ -139,7 +166,9 @@ export const projects = [
     image: 'canvas.png',
     link: 'https://canvas.dirkb.cyou/',
     summary: 'Testomgeving',
-    details: '',
+    details: `Testomgeving met de open source versie van Canvas LMS.
+    | Oracle Cloud Instance.
+    | Virtualhost met toekenning domeinnaam.`,
   },
   {
     id: 16,
@@ -148,6 +177,8 @@ export const projects = [
     image: 'justdoit.png',
     link: 'https://gcp.dirkb.cyou:3000/',
     summary: 'Todo applicatie',
-    details: '',
+    details: `Opdracht voor Arteveldehogeschool. 
+    | Een todo-applicatie met Express, TypeORM (sqlite3), Handlebars, authenticatie met JSON Web Token.
+    | Staat online op een Google Cloud Instance`,
   },
 ];
